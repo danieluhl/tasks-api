8a867f831aa96458687f719e7d36f8b28bc7ab6d
Dan Uhl <dan.uhl@posh.tech>
2024-10-16 12:00:26 -0400

    adds initial tests

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,28 @@
 import { drizzle } from "drizzle-orm/connect";
+import { newDb } from "pg-mem";
 
 import { env } from "@/env";
 
 import * as schema from "./schema";
 
-const db = await drizzle("node-postgres", {
-  connection: {
-    connectionString: env.DATABASE_URL,
-  },
-  schema,
-});
+async function createDb() {
+  if (env.NODE_ENV === "testing") {
+    const inMemDb = newDb().adapters.createPg().Client;
+    const db = drizzle(inMemDb);
+    // await migrate(db, { migrationsFolder: "../migrations" }).catch((err) => {
+
+    // });
+    return db;
+  } else {
+    return await drizzle("node-postgres", {
+      connection: {
+        connectionString: env.DATABASE_URL,
+      },
+      schema,
+    });
+  }
+}
+
+const db = await createDb();
 
 export { db };
